db36f58f66dea3fb63598129e32db9e1d3874aa3
ToanRayZa <rayza19102006@gmail.com>
2026-05-31 22:41:43 +0700

    JP18: Tìm kiếm bài báo

--- backend/com.journaltracker/src/main/java/com/journaltracker/dto/response/ApiResponse.java
+++ backend/com.journaltracker/src/main/java/com/journaltracker/dto/response/ApiResponse.java
@@ -1,43 +1,15 @@
 package com.journaltracker.dto.response;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@Builder
-@NoArgsConstructor
 @AllArgsConstructor
 public class ApiResponse<T> {
+
     private boolean success;
+
     private String message;
-    private T body;
-    public static <T> ApiResponse<T> success(
-            String message,
-            T body
-    ) {
-        return ApiResponse.<T>builder()
-                .success(true)
-                .message(message)
-                .body(body)
-                .build();
-    }
-    public static <T> ApiResponse<T> success(T body) {
-        return success("success", body);
-    }
-    public static <T> ApiResponse<T> error(String message, T body) {
-        return ApiResponse.<T>builder()
-                .success(false)
-                .message(message)
-                .body(body)
-                .build();
-    }
-    public static <T> ApiResponse<T> error(String message) {
-        return ApiResponse.<T>builder()
-                .success(false)
-                .message(message)
-                .body(null)
-                .build();
-    }
+
+    private T data;
 }
--- backend/com.journaltracker/src/main/java/com/journaltracker/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.journaltracker.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-  public ResourceNotFoundException(String message) {
-    super(message);
-  }
-}
